d34fa0e0bf8677529d215c5c767db6af9f281c79
Mohammed EL-IDRISSI <ingmedical16@gmail.com>
2024-07-03 01:48:00 +0200

    Technique d’animation de sprite

---
+++ src/static/db.js
@@ -0,0 +1,43 @@
+
+export const dogAnimationStates = [
+    {
+      name: 'idle',
+      frames: 7,
+    },
+    {
+      name: 'jump',
+      frames: 7,
+    },
+    {
+      name: 'fail',
+      frames: 7,
+    },
+    {
+      name: 'run',
+      frames: 9,
+    },
+    {
+      name: 'dizzy',
+      frames: 11,
+    },
+    {
+      name: 'sit',
+      frames: 5,
+    },
+    {
+      name: 'roll',
+      frames: 7,
+    },
+    {
+      name: 'bite',
+      frames: 7,
+    },
+    {
+      name: 'ko',
+      frames: 12,
+    },
+    {
+      name: 'getHit',
+      frames: 4,
+    }
+  ];
